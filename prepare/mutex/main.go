329f55c3b81615232fc4c0bc62ad8232696e4a7c
haolipeng <1078285863@qq.com>
2021-04-22 22:56:30 +0800

    mutex ref and pointer testing

--- prepare/mutex/main.go
+++ prepare/mutex/main.go
@@ -11,15 +11,35 @@ type Student struct {
 	name string
 }
 
-func (s Student) add() {
+//结论：以值传递方式使用mutex，会复制其锁的状态，但是操作的并不是同一把锁，切记！！！
+
+func (s Student) UnlockWithRef() {
 	//输出mutex的值
-	fmt.Println("mutex:", s.mu)
+	fmt.Printf("object: %p before mutex.UnlockWithRef() mutext:%v\n", &s, s.mu)
+	s.mu.Unlock()
+	fmt.Printf("object: %p after mutex.UnlockWithRef() mutext:%v\n", &s, s.mu)
+}
+
+func (s *Student) UnlockWithPointer() {
+	//输出mutex的值
+	fmt.Printf("object: %p before mutex.UnlockWithPointer() mutext:%v\n", &s, s.mu)
+	s.mu.Unlock()
+	fmt.Printf("object: %p after mutex.UnlockWithPointer() mutext:%v\n", &s, s.mu)
 }
 
 func main() {
-	var stu Student
+	/*var stu Student
 	stu.mu.Lock()
-	fmt.Println("origin mutex:", stu.mu)
-	stu.add()
-	stu.mu.Unlock()
+
+	//经过两次解锁后，看看是否为空
+	fmt.Println("origin mutex:", &stu.mu)
+	stu.UnlockWithRef()
+	stu.UnlockWithRef()*/
+
+	var stu2 Student
+	stu2.mu.Lock()
+
+	fmt.Println("origin mutex:", &stu2.mu)
+	stu2.UnlockWithPointer()
+	stu2.UnlockWithPointer() //调用两次unlock函数会触发如下错误，sync: unlock of unlocked mutex
 }
