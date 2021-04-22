fd9a66dc75c96ca1b0806b8910af6f31d0a85006
haolipeng <1078285863@qq.com>
2021-04-22 22:58:24 +0800

    fix bug:method operate mutex must with object pointer

--- day2-single-node/geecache/cache.go
+++ day2-single-node/geecache/cache.go
@@ -12,9 +12,8 @@ type cache struct {
 	cacheBytes int64
 }
 
-//add函数 fix bug:c cache -> c *cache
-//TODO:方法在传指针和传对象有啥区别呢?
-func (c cache) add(key string, value ByteView) {
+//add函数 fix bug:c cache -> c *cache,receiver 必须使用指针类型
+func (c *cache) add(key string, value ByteView) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
@@ -26,7 +25,7 @@ func (c cache) add(key string, value ByteView) {
 }
 
 //get函数,返回获取结果 fix bug:c cache -> c *cache
-func (c cache) get(key string) (ByteView, bool) {
+func (c *cache) get(key string) (ByteView, bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
